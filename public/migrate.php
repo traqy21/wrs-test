0c563cb678a51248d6ca36a53d5bbcc51d6b950d
Aries <traqy21@gmail.com>
2020-12-13 00:13:05 +0800

    remove commented codes

--- public/migrate.php
+++ public/migrate.php
@@ -1,28 +1,7 @@
 <?php
 include ('db.php');
 
-$db = new db();
-
-//$createParentTable = "
-//    CREATE TABLE IF NOT EXISTS parents (
-//        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-//        name varchar(16) NOT NULL
-//    )
-//";
-//
-//$createChildTable = "
-//    CREATE TABLE IF NOT EXISTS children (
-//        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-//        name VARCHAR(16) NOT NULL,
-//        parent_id INT UNSIGNED NOT NULL,
-//        FOREIGN KEY (parent_id)
-//            REFERENCES parents (id)
-//            ON DELETE CASCADE
-//    );
-//";
-//
-//$db->execute($createParentTable);
-//$db->execute($createChildTable);
+$db = new db(); 
 
 $createNodesTable = "
     CREATE TABLE IF NOT EXISTS nodes (
